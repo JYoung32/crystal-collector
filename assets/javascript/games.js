bfe27b260fc8172d8fb6b7aceaed1c502eef8892
Joseph Young <joseph.young32@yahoo.com>
2019-09-13 13:33:05 -0400

    finished basic wireframe layout, added psuedocode to js file

---
+++ assets/javascript/games.js
@@ -0,0 +1,5 @@
+// 1- when page loads get random number for 'Points Needed:' ; value 19 - 120;
+// 2- when page loads have each crystal get an individual random number; value 1 - 12; keep value hidden from user
+// 3- when a crystal is clicked, add the random number to 'Current points"
+// 4- if user score is the same as random number; user wins; if score goes above random number, user loses.
+// 5- when win/loss, game restarts. on restart, load new random number to achieve and randomize 4 crystal values

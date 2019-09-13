72ec54aaee41eca695b06911113f5fa56c10c3da
Joseph Young <joseph.young32@yahoo.com>
2019-09-13 14:53:08 -0400

    got random number to generate and 4 random values to generate for crystals

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -3,3 +3,49 @@
 // 3- when a crystal is clicked, add the random number to 'Current points"
 // 4- if user score is the same as random number; user wins; if score goes above random number, user loses.
 // 5- when win/loss, game restarts. on restart, load new random number to achieve and randomize 4 crystal values
+$(document).ready(function(){
+//variable to store data onto html page
+var randomNumber = randomNum(19,120);
+var currentNumber = 0;
+var wins = "";
+var loses = "";
+var crystalOne = crystalNum();
+var crystalTwo = crystalNum();
+var crystalThree = crystalNum();
+var crystalFour = crystalNum();
+
+
+
+
+
+//make a function to generate random number between 19 - 120
+function randomNum(min, max){
+    return randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+};
+console.log(randomNumber);
+$("#randomNumber").html(randomNumber);
+//make a function to generate a random number for crystals; value 1-12
+function crystalNum(){
+    return Math.floor(Math.random() * 12) + 1;
+}
+$("#crystalOne").val(crystalOne);
+$("#crystalTwo").val(crystalTwo);
+$("#crystalThree").val(crystalThree);
+$("#crystalFour").val(crystalFour);
+
+
+$("#crystalOne").on("click", function(){
+    $("#currentPoints).html()
+})
+console.log(crystalOne);
+console.log(crystalTwo);
+console.log(crystalThree);
+console.log(crystalFour);
+//append random value to 4 crystals
+
+//click events for each crystal
+$("#crystalOne").on('click', function(){
+
+});
+
+});
